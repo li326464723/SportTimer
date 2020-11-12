7238fe31045f8b062f10d3c30fad68a4119cb233
p_zcli <p_zcli@tencent.com>
2020-11-12 16:12:57 +0800

    增加配置按钮

--- app/src/main/java/com/lowry/sporttimer/MainActivity.java
+++ app/src/main/java/com/lowry/sporttimer/MainActivity.java
@@ -3,12 +3,26 @@ package com.lowry.sporttimer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button:
+                Toast.makeText(this, "hahahah", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+
     }
 }
